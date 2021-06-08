9227f6867937a5d9e0b5f4904374f0a7034ff8e3
Kylie Serocki <>
2021-06-08 14:32:54 +0000

    6/8/21

--- date.js
+++ date.js
@@ -3,3 +3,11 @@ function getDate()
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
 }
+function blue()
+{
+	document.getElementById("date").style.color="blue";
+}
+function red()
+{
+	document.getElementById("date").style.color="red";
+}
