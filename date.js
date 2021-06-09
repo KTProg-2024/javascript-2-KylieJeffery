743da4dc97d5635d238bedd2a0462deb535ff9f9
Kylie Serocki <78364266+KylieJeffery@users.noreply.github.com>
2021-06-09 12:48:16 +0000

    6/9/21

--- date.js
+++ date.js
@@ -6,8 +6,14 @@ function getDate()
 function blue()
 {
 	document.getElementById("date").style.color="blue";
+  document.getElementById("date").style.font = "italic bold 20px arial,serif";
 }
 function red()
 {
 	document.getElementById("date").style.color="red";
+  document.getElementById("date").style.font = "italic bold 20px arial,serif";
+}
+function update() {
+ var d = new Date();
+  document.getElementById("date").innerHTML = d;
 }
